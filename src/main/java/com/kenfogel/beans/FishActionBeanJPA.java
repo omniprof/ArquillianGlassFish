7a6d73b24b222382376965e1e8bb9ce454aade85
Ken Fogel <omniprof@gmail.com>
2015-01-21 13:18:39 -0500

    Here is my code so far. identical to last year but now using Glassfish rather than TomEE

---
+++ src/main/java/com/kenfogel/beans/FishActionBeanJPA.java
@@ -0,0 +1,48 @@
+package com.kenfogel.beans;
+
+import com.kenfogel.entities.Fish;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+
+/**
+ * A magic bean that returns a list of fish
+ *
+ * @author Ken
+ *
+ */
+@Named
+@RequestScoped
+public class FishActionBeanJPA implements Serializable {
+
+    private static final long serialVersionUID = 7498237763098271958L;
+    @Resource
+    private UserTransaction userTransaction;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Fish> getAll() throws SQLException {
+
+        // Criteria for Select all records
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Fish> cq = cb.createQuery(Fish.class);
+        Root<Fish> fish = cq.from(Fish.class);
+        cq.select(fish);
+        TypedQuery<Fish> query = entityManager.createQuery(cq);
+        List<Fish> fishies = query.getResultList();
+
+        return fishies;
+    }
+}
---
+++ src/test/java/com/kenfogel/test/FishActionBeanTest.java
@@ -0,0 +1,139 @@
+/*
+ * A sample test class using Arquillian
+ */
+
+package com.kenfogel.test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.kenfogel.beans.FishActionBeanJPA;
+import com.kenfogel.entities.Fish;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+@RunWith(Arquillian.class)
+public class FishActionBeanTest {
+
+	@Deployment
+	public static WebArchive deploy() {
+
+		// Use an alternative to the JUnit assert library called AssertJ
+		// Need to reference MySQL driver as it is not part of either
+		// embedded or remote TomEE
+		final File[] dependencies = Maven
+				.resolver()
+				.loadPomFromFile("pom.xml")
+				.resolve("mysql:mysql-connector-java",
+						"org.assertj:assertj-core").withoutTransitivity()
+				.asFile();
+
+		// For testing Arquillian prefers a resources.xml file over a
+		// context.xml
+		// Actual file name is resources-mysql-ds.xml in the test/resources
+		// folder
+		// The SQL script to create the database is also in this folder
+
+		final WebArchive webArchive = ShrinkWrap.create(WebArchive.class)
+				.addPackage(FishActionBeanJPA.class.getPackage())
+				.addPackage(Fish.class.getPackage())
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				.addAsWebInfResource("glassfish-resources.xml", "resources.xml")
+				.addAsWebInfResource("META-INF/persistence.xml", "persistence.xml")
+				.addAsResource("createFishMySQL.sql")
+				.addAsLibraries(dependencies);
+                
+                
+		return webArchive;
+	}
+
+	@Inject
+	private FishActionBeanJPA fab;
+
+//    // Used to rebuild the database
+//    @Resource(name = "jdbc/mydata")
+//    private DataSource ds;
+//
+//    /**
+//     * This routine is courtesy of Bartosz Majsak who also solved my Arquillian
+//     * remote server problem
+//     */
+//    //@Before
+//    public void seedDatabase() {
+//        final String seedDataScript = loadAsString("createFishMySQL.sql");
+//        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Aquarium", "fish", "fish")) {
+//            for (String statement : splitStatements(new StringReader(
+//                    seedDataScript), ";")) {
+//                connection.prepareStatement(statement).execute();
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Failed seeding database", e);
+//        }
+//    }
+
+	@Test
+	public void should_find_all_fish() throws Exception {
+		List<Fish> lfd = fab.getAll();
+		assertThat(lfd).hasSize(200);
+	}
+
+//    /**
+//     * The following methods support the seedDatabse method
+//     */
+//    private String loadAsString(final String path) {
+//        try (InputStream inputStream = Thread.currentThread()
+//                .getContextClassLoader().getResourceAsStream(path)) {
+//            return new Scanner(inputStream).useDelimiter("\\A").next();
+//        } catch (IOException e) {
+//            throw new RuntimeException("Unable to close input stream.", e);
+//        }
+//    }
+//
+//    private List<String> splitStatements(Reader reader,
+//            String statementDelimiter) {
+//        final BufferedReader bufferedReader = new BufferedReader(reader);
+//        final StringBuilder sqlStatement = new StringBuilder();
+//        final List<String> statements = new LinkedList<>();
+//        try {
+//            String line = "";
+//            while ((line = bufferedReader.readLine()) != null) {
+//                line = line.trim();
+//                if (line.isEmpty() || isComment(line)) {
+//                    continue;
+//                }
+//                sqlStatement.append(line);
+//                if (line.endsWith(statementDelimiter)) {
+//                    statements.add(sqlStatement.toString());
+//                    sqlStatement.setLength(0);
+//                }
+//            }
+//            return statements;
+//        } catch (IOException e) {
+//            throw new RuntimeException("Failed parsing sql", e);
+//        }
+//    }
+//
+//    private boolean isComment(final String line) {
+//        return line.startsWith("--") || line.startsWith("//")
+//                || line.startsWith("/*");
+//    }
+
+}
